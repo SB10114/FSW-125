34c86dc30ebb184fec2ae11c4c85ec625e195f7a
Sierra <sierra.butler@bryanuniversity.edu>
2021-09-03 19:01:04 -0500

    week1

---
+++ week1/math/app.js
@@ -0,0 +1,15 @@
+// const typeSum = require('./math');
+
+// console.log(typeSum(5, 3));
+
+// const typeSubt = require('./math');
+
+// console.log(typeSubt(50, 35));
+
+// const typeMult = require('./math');
+
+// console.log(typeMult(8, 4));
+
+const typeDivide = require('./math');
+
+console.log(typeDivide(15, 3));
---
+++ week1/math/math.js
@@ -0,0 +1,23 @@
+// const sum = (a , b) => {
+//     return a + b
+// }
+
+// module.exports = sum
+
+// const subtract= (a , b) => {
+//     return a - b
+// }
+
+//  module.exports = subtract
+
+//const multiplication = (a , b) => {
+//     return a * b
+// }
+
+// module.exports = multiplication
+
+const division = (a , b) => {
+    return a / b
+}
+
+module.exports = division
